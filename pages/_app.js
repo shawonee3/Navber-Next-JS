c8356fcf549738aa71848d714a3b72bcbed08a34
shawonee3 <161241255+shawonee3@users.noreply.github.com>
2024-02-25 23:51:02 +0600

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }) {
+
+  return (
+    <div>
+      <Header/>
+      <Component {...pageProps} />;
+    </div>
+  )
+     
+   
+}
---
+++ pages/login.js
@@ -0,0 +1,51 @@
+import React from 'react';
+
+function Login() {
+  return (
+    <div className="w-full container mx-auto max-w-md px-2 justify-center mt-5">
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+            Username
+          </label>
+          <input
+            id="username"
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="text"
+            placeholder="Username"
+          />
+        </div>
+        <div className="mb-6">
+          <label htmlFor="password" className="block text-gray-700 text-sm font-bold mb-2">
+            Password
+          </label>
+          <input
+            id="password"
+            className="shadow appearance-none border border-gray-300 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            placeholder="******************"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit" // Changed type to "submit"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            href="#"
+          >
+            Forgot Password?
+          </a>
+        </div>
+      </form>
+      <p className="text-center text-gray-500 text-xs">
+        &copy;2024 Shawon. All rights reserved.
+      </p>
+    </div>
+  );
+}
+
+export default Login;
